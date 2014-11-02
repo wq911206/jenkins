cd809e314474768d862140efb1f34824f9b1235d
wq <wq911206@gmail.com>
2014-11-02 09:43:33 -0600

    Add a failure test

--- test/authentication_tests.py
+++ test/authentication_tests.py
@@ -27,3 +27,6 @@ class StandAloneTests(TestCase):
         """Test the login function when an error happens."""
         mock_open.side_effect = IOError()
         self.assertFalse(auth.login('george', 'bosco'))
+    def test_logout(self):
+        """Test the logout function...badly."""
+        self.assertEqual(0, 1)
